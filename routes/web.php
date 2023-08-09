fdc2c06be7ba3b2f9af7b7fcb787f40f2afb8c05
sontung04 <sontung291004@gmail.com>
2023-08-09 20:04:08 +0700

    router

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::prefix('post') -> group( function () {
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    Route::get('/index' , function(){ //hiển thị danh sách post trong blog
 
-Route::get('/', function () {
-    return view('welcome');
+    });
+
+    Route::get('/create' , function() { //hiển thị form để thêm 1 post
+       
+    });
+
+    Route::post('/create' , function() { //nhận dữ liệu tạo mới post từ form
+        
+    });
+
+    Route::get('/detail/{id}' , function($id) { //hiển thị nội dung chi tiết của 1 post theo id
+        
+    });
+
+    Route::get('/update/{id}' , function($id) { //hiển thị form cập nhật nội dung theo id
+
+    });
+
+    Route::post('/update/{id}' , function($id){ //nhận dữ liệu cập nhật nội dùng post từ form
+
+    });
+
+    Route::post('/delete/{id}' , function($id){ //Xóa 1 post theo id
+
+    });
+
 });
